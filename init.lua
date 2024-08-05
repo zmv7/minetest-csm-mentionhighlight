10e22c2f80b98792b4820d125620b6803391eb4c
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2024-08-05 20:29:06 +0500

    Fix assignment to global variable `matched`

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
-minetest.register_on_receiving_chat_message(function(msg)
+minetest.register_on_receiving_chat_message(function(message)
 	local name = minetest.localplayer and minetest.localplayer:get_name()
-	if not (name and msg) then return end
-	local sender, text = msg:match("<?(%S+)[:?>?]%s?(.+)")
+	if not (name and message) then return end
+	local sender, text = message:match("<?(%S+)[:?>?]%s?(.+)")
 	if sender and sender == name then return end
-	msg, matched = msg:gsub(name, minetest.colorize("#F00",name))
+	local msg, matched = message:gsub(name, minetest.colorize("#F00",name))
 	if matched and matched > 0 then
 		minetest.display_chat_message(msg)
 		return true
